5259f194b659b5b5a1fa5330a87a696b5a827406
citysiva180@gmail.com <citysiva180@gmail.com>
2022-02-10 06:08:17 +0530

    Initial Commit

--- main.py
+++ main.py
@@ -2,13 +2,21 @@
 from flask import Flask
 from flask_restful import Api, Resource
 
+
+# Instance of a flask app  and wrapping the app in api
 app = Flask(__name__)
 api = Api(app)
 
 
+# creation of class routes
 class HelloWorld(Resource):
-    pass
+    def get(self):
+        return{"data": "Hello World"}
 
 
+api.add_resource(HelloWorld, "/helloworld")
+
+
+# Check if the app is main, and start from there...
 if __name__ == "__main__":
     app.run(debug=True)
